77f039fac22c4c8ccf3d90c46ba3b53e5f1eb65c
tale <2439082470@qq.com>
2024-06-16 12:38:49 +0800

    Resolved merge conflicts

---
+++ src/test/java/matrixversion/queryBridgeWordTest.java
@@ -0,0 +1,53 @@
+package matrixversion;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class queryBridgeWordTest {
+    public static GraMethod graMethod = new GraMethod();
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        //graMethod.textToGraph("C:\\Users\\tale\\Documents\\Tencent Files\\2439082470\\FileRecv\\MobileFile\\softwareproject1\\softwareproject1\\test.txt");
+    }
+
+    @Test
+    public void test() {
+        graMethod.textToGraph("C:\\Users\\tale\\Documents\\Tencent Files\\2439082470\\FileRecv\\MobileFile\\softwareproject1\\softwareproject1\\src\\test\\java\\text\\txt\\test1.txt");
+        String result = graMethod.queryBridgeWords("word","mother");
+        assertEquals("No word or mother in the graph!",result);
+    }
+
+    @Test
+    public void test2() {
+        graMethod.textToGraph("C:\\Users\\tale\\Documents\\Tencent Files\\2439082470\\FileRecv\\MobileFile\\softwareproject1\\softwareproject1\\src\\test\\java\\text\\txt\\test1.txt");
+        String result = graMethod.queryBridgeWords("morning","mother");
+        assertEquals("No morning or mother in the graph!",result);
+
+    }
+    @Test
+    public void test3() {
+        graMethod.textToGraph("src/test/java/text/txt/test2.txt");
+        String result = graMethod.queryBridgeWords("second","first");
+        assertEquals("No bridge words from second to first!",result);
+
+    }
+    @Test
+    public void test4() {
+        graMethod.textToGraph("src/test/java/text/txt/test2.txt");
+        String result = graMethod.queryBridgeWords("first","second");
+        assertEquals("No bridge words from first to second!",result);
+    }
+    @Test
+    public void test5() {
+        graMethod.textToGraph("src/test/java/text/txt/test3.txt");
+        String result = graMethod.queryBridgeWords("first","third");
+        assertEquals("The bridge word from first to third is:second",result);
+    }
+    @Test
+    public void test6() {
+        graMethod.textToGraph("src/test/java/text/txt/test4.txt");
+        String result = graMethod.queryBridgeWords("first","third");
+        assertEquals("The bridge words from first to third are:second and forth",result);
+    }
+}
